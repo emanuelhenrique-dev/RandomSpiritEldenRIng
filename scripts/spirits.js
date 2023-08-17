6fe9d42d13c5917c12f8b09e85ec514e466191b9
unknown <emanuelhenriquefs@gmail.com>
2023-08-16 23:41:57 -0300

    fix2

--- scripts/spirits.js
+++ scripts/spirits.js
@@ -2,448 +2,448 @@ const spiritList = [
   {
     id: '0',
     name: 'WANDERING NOBLE ASHES',
-    img: '../assets/wandering_noble_ashes.png',
+    img: './assets/wandering_noble_ashes.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Wandering+Noble+Ashes'
   },
   {
     id: '1',
     name: 'NOBLE SORCERER ASHES',
-    img: '../assets/NOBLE_SORCERER_ASHES.png',
+    img: './assets/NOBLE_SORCERER_ASHES.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Noble+Sorcerer+Ashes'
   },
   {
     id: '2',
     name: 'NOMAD ASHES',
-    img: '../assets/NOMAD_ASHES.png',
+    img: './assets/NOMAD_ASHES.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Nomad+Ashes'
   },
   {
     id: '3',
     name: 'PUTRID CORPSE ASHES',
-    img: '../assets/PUTRID_CORPSE_ASHES.png',
+    img: './assets/PUTRID_CORPSE_ASHES.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Putrid+Corpse+Ashes'
   },
   {
     id: '4',
     name: 'SKELETAL MILITIAMAN ASHES',
-    img: '../assets/SKELETAL_MILITIAMAN_ASHES.png',
+    img: './assets/SKELETAL_MILITIAMAN_ASHES.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Skeletal+Militiaman+Ashes'
   },
   {
     id: '5',
     name: 'SKELETAL BANDIT ASHES',
-    img: '../assets/SKELETAL_BANDIT_ASHES.png',
+    img: './assets/SKELETAL_BANDIT_ASHES.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Skeletal+Bandit+Ashes'
   },
   {
     id: '6',
     name: 'ALBINAURIC ASHES',
-    img: '../assets/ALBINAURIC_ASHES.png',
+    img: './assets/ALBINAURIC_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Albinauric+Ashes'
   },
   {
     id: '7',
     name: 'WINGED MISBEGOTTEN ASHES',
-    img: '../assets/WINGED_MISBEGOTTEN_ASHES.png',
+    img: './assets/WINGED_MISBEGOTTEN_ASHES.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Winged+Misbegotten+Ashes'
   },
   {
     id: '8',
     name: 'DEMI-HUMAN ASHES',
-    img: '../assets/DEMI-HUMAN_ASHES.png',
+    img: './assets/DEMI-HUMAN_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Demi-Human+Ashes'
   },
   {
     id: '9',
     name: 'CLAYMAN ASHES',
-    img: '../assets/CLAYMAN_ASHES.png',
+    img: './assets/CLAYMAN_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Clayman+Ashes'
   },
   {
     id: '10',
     name: 'ORACLE ENVOY ASHES',
-    img: '../assets/ORACLE_ENVOY_ASHES.png',
+    img: './assets/ORACLE_ENVOY_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Oracle+Envoy+Ashes'
   },
   {
     id: '11',
     name: 'LONE WOLF ASHES',
-    img: '../assets/LONE_WOLF_ASHES.png',
+    img: './assets/LONE_WOLF_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Lone+Wolf+Ashes'
   },
   {
     id: '12',
     name: 'ROTTEN STRAY ASHES',
-    img: '../assets/ROTTEN_STRAY_ASHES.png',
+    img: './assets/ROTTEN_STRAY_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Rotten+Stray+Ashes'
   },
   {
     id: '13',
     name: 'GIANT RAT ASHES',
-    img: '../assets/GIANT_RAT_ASHES.png',
+    img: './assets/GIANT_RAT_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Giant+Rat+Ashes'
   },
   {
     id: '14',
     name: 'WARHAWK ASHES',
-    img: '../assets/WARHAWK_ASHES.png',
+    img: './assets/WARHAWK_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Warhawk+Ashes'
   },
   {
     id: '15',
     name: 'LAND SQUIRT ASHES',
-    img: '../assets/LAND_SQUIRT_ASHES.png',
+    img: './assets/LAND_SQUIRT_ASHES.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Land+Squirt+Ashes'
   },
   {
     id: '16',
     name: 'SPIRIT JELLYFISH ASHES',
-    img: '../assets/SPIRIT_JELLYFISH_ASHES.png',
+    img: './assets/SPIRIT_JELLYFISH_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Spirit+Jellyfish+Ashes'
   },
   {
     id: '17',
     name: 'MIRANDA SPROUT ASHES',
-    img: '../assets/MIRANDA_SPROUT_ASHES.png',
+    img: './assets/MIRANDA_SPROUT_ASHES.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Miranda+Sprout+Ashes'
   },
   {
     id: '18',
     name: 'FANGED IMP ASHES',
-    img: '../assets/FANGED_IMP_ASHES.png',
+    img: './assets/FANGED_IMP_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Fanged+Imp+Ashes'
   },
   {
     id: '19',
     name: 'SOLDJARS OF FORTUNE ASHES',
-    img: '../assets/SOLDJARS_OF_FORTUNE_ASHES.png',
+    img: './assets/SOLDJARS_OF_FORTUNE_ASHES.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Soldjars+of+Fortune+Ashes'
   },
   {
     id: '20',
     name: 'ARCHER ASHES',
-    img: '../assets/ARCHER_ASHES.png',
+    img: './assets/ARCHER_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Archer+Ashes'
   },
   {
     id: '21',
     name: 'GREATSHIELD SOLDIER ASHES',
-    img: '../assets/GREATSHIELD_SOLDIER_ASHES.png',
+    img: './assets/GREATSHIELD_SOLDIER_ASHES.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Greatshield+Soldier+Ashes'
   },
   {
     id: '22',
     name: 'PAGE ASHES',
-    img: '../assets/PAGE_ASHES.png',
+    img: './assets/PAGE_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Page+Ashes'
   },
   {
     id: '23',
     name: 'VULGAR MILITIA ASHES',
-    img: '../assets/VULGAR_MILITIA_ASHES.png',
+    img: './assets/VULGAR_MILITIA_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Vulgar+Militia+Ashes'
   },
   {
     id: '24',
     name: 'MARIONETTE SOLDIER ASHES',
-    img: '../assets/MARIONETTE_SOLDIER_ASHES.png',
+    img: './assets/MARIONETTE_SOLDIER_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Marionette+Soldier+Ashes'
   },
   {
     id: '25',
     name: 'AVIONETTE SOLDIER ASHES',
-    img: '../assets/AVIONETTE_SOLDIER_ASHES.png',
+    img: './assets/AVIONETTE_SOLDIER_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Avionette+Soldier+Ashes'
   },
   {
     id: '26',
     name: 'KAIDEN SELLSWORD ASHES',
-    img: '../assets/KAIDEN_SELLSWORD_ASHES.png',
+    img: './assets/KAIDEN_SELLSWORD_ASHES.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Kaiden+Sellsword+Ashes'
   },
   {
     id: '27',
     name: 'MAD PUMPKIN HEAD ASHES',
-    img: '../assets/MAD_PUMPKIN_HEAD_ASHES.png',
+    img: './assets/MAD_PUMPKIN_HEAD_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Mad+Pumpkin+Head+Ashes'
   },
   {
     id: '28',
     name: 'FIRE MONK ASHES',
-    img: '../assets/FIRE_MONK_ASHES.png',
+    img: './assets/FIRE_MONK_ASHES.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Fire+Monk+Ashes'
   },
   {
     id: '29',
     name: 'ANCESTRAL FOLLOWER ASHES',
-    img: '../assets/ANCESTRAL_FOLLOWER_ASHES.png',
+    img: './assets/ANCESTRAL_FOLLOWER_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Ancestral+Follower+Ashes'
   },
   {
     id: '30',
     name: 'AZULA BEASTMAN ASHES',
-    img: '../assets/AZULA_BEASTMAN_ASHES.png',
+    img: './assets/AZULA_BEASTMAN_ASHES.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Azula+Beastman+Ashes'
   },
   {
     id: '31',
     name: 'MAN-SERPENT ASHES',
-    img: '../assets/MAN-SERPENT_ASHES.png',
+    img: './assets/MAN-SERPENT_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Man-Serpent+Ashes'
   },
   {
     id: '32',
     name: 'CRYSTALIAN ASHES',
-    img: '../assets/CRYSTALIAN_ASHES.png',
+    img: './assets/CRYSTALIAN_ASHES.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Crystalian+Ashes'
   },
   {
     id: '33',
     name: 'KINDRED OF ROT ASHES',
-    img: '../assets/KINDRED_OF_ROT_ASHES.png',
+    img: './assets/KINDRED_OF_ROT_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Kindred+of+Rot+Ashes'
   },
   {
     id: '34',
     name: 'GLINTSTONE SORCERER ASHES',
-    img: '../assets/GLINTSTONE_SORCERER_ASHES.png',
+    img: './assets/GLINTSTONE_SORCERER_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Glintstone+Sorcerer+Ashes'
   },
   {
     id: '35',
     name: 'TWINSAGE SORCERER ASHES',
-    img: '../assets/TWINSAGE_SORCERER_ASHES.png',
+    img: './assets/TWINSAGE_SORCERER_ASHES.png',
     dif: '4',
     Link: 'https://eldenring.wiki.fextralife.com/Twinsage+Sorcerer+Ashes'
   },
   {
     id: '36',
     name: 'GODRICK SOLDIER ASHES',
-    img: '../assets/GODRICK_SOLDIER_ASHES.png',
+    img: './assets/GODRICK_SOLDIER_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Godrick+Soldier+Ashes'
   },
   {
     id: '37',
     name: 'RAYA LUCARIA SOLDIER ASHES',
-    img: '../assets/RAYA_LUCARIA_SOLDIER_ASHES.png',
+    img: './assets/RAYA_LUCARIA_SOLDIER_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Raya+Lucaria+Soldier+Ashes'
   },
   {
     id: '38',
     name: 'LEYNDELL SOLDIER ASHES',
-    img: '../assets/LEYNDELL_SOLDIER_ASHES.png',
+    img: './assets/LEYNDELL_SOLDIER_ASHES.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Leyndell+Soldier+Ashes'
   },
   {
     id: '39',
     name: 'RADAHN SOLDIER ASHES',
-    img: '../assets/RADAHN_SOLDIER_ASHES.png',
+    img: './assets/RADAHN_SOLDIER_ASHES.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Radahn+Soldier+Ashes'
   },
   {
     id: '40',
     name: 'HALIGTREE SOLDIER ASHES',
-    img: '../assets/HALIGTREE_SOLDIER_ASHES.png',
+    img: './assets/HALIGTREE_SOLDIER_ASHES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Haligtree+Soldier+Ashes'
   },
   {
     id: '41',
     name: 'MAUSOLEUM SOLDIER ASHES',
-    img: '../assets/MAUSOLEUM_SOLDIER_ASHES.png',
+    img: './assets/MAUSOLEUM_SOLDIER_ASHES.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Mausoleum+Soldier+Ashes'
   },
   {
     id: '42',
     name: 'STORMHAWK DEENH',
-    img: '../assets/STORMHAWK_DEENH.png',
+    img: './assets/STORMHAWK_DEENH.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Stormhawk+Deenh'
   },
   {
     id: '43',
     name: 'BANISHED KNIGHT OLEG',
-    img: '../assets/BANISHED_KNIGHT_OLEG.png',
+    img: './assets/BANISHED_KNIGHT_OLEG.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Banished+Knight+Oleg+Ashes'
   },
   {
     id: '44',
     name: 'BANISHED KNIGHT ENGVALL',
-    img: '../assets/BANISHED_KNIGHT_ENGVALL.png',
+    img: './assets/BANISHED_KNIGHT_ENGVALL.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Banished+Knight+Engvall+Ashes'
   },
   {
     id: '45',
     name: 'BLOODHOUND KNIGHT FLOH',
-    img: '../assets/BLOODHOUND_KNIGHT_FLOH.png',
+    img: './assets/BLOODHOUND_KNIGHT_FLOH.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Bloodhound+Knight+Floh'
   },
   {
     id: '46',
     name: 'BATTLEMAGE HUGUES ASHES',
-    img: '../assets/BATTLEMAGE_HUGUES_ASHES.png',
+    img: './assets/BATTLEMAGE_HUGUES_ASHES.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Battlemage+Hugues+Ashes'
   },
   {
     id: '47',
     name: 'LATENNA THE ALBINAURIC',
-    img: '../assets/LATENNA_THE_ALBINAURIC.png',
+    img: './assets/LATENNA_THE_ALBINAURIC.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Latenna+The+Albinauric'
   },
   {
     id: '48',
     name: 'PERFUMER TRICIA',
-    img: '../assets/PERFUMER_TRICIA.png',
+    img: './assets/PERFUMER_TRICIA.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Perfumer+Tricia+Ashes'
   },
   {
     id: '49',
     name: 'DEPRAVED PERFUMER CARMAAN ASHES',
-    img: '../assets/DEPRAVED_PERFUMER_CARMAAN_ASHES.png',
+    img: './assets/DEPRAVED_PERFUMER_CARMAAN_ASHES.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Depraved+Perfumer+Carmaan+Ashes'
   },
   {
     id: '50',
     name: 'OMENKILLER ROLLO',
-    img: '../assets/OMENKILLER_ROLLO.png',
+    img: './assets/OMENKILLER_ROLLO.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Omenkiller+Rollo'
   },
   {
     id: '51',
     name: 'BLACKFLAME MONK AMON ASHES',
-    img: '../assets/BLACKFLAME_MONK_AMON_ASHES.png',
+    img: './assets/BLACKFLAME_MONK_AMON_ASHES.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Blackflame+Monk+Amon+Ashes'
   },
   {
     id: '52',
     name: 'ANCIENT DRAGON KNIGHT KRISTOFF ASHES',
-    img: '../assets/ANCIENT_DRAGON_KNIGHT_KRISTOFF_ASHES.png',
+    img: './assets/ANCIENT_DRAGON_KNIGHT_KRISTOFF_ASHES.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Ancient+Dragon+Knight+Kristoff+Ashes'
   },
   {
     id: '53',
     name: 'REDMANE KNIGHT OGHA',
-    img: '../assets/REDMANE_KNIGHT_OGHA.png',
+    img: './assets/REDMANE_KNIGHT_OGHA.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Redmane+Knight+Ogha+Ashes'
   },
   {
     id: '54',
     name: 'LHUTEL THE HEADLESS',
-    img: '../assets/LHUTEL_THE_HEADLESS.png',
+    img: './assets/LHUTEL_THE_HEADLESS.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Lhutel+the+Headless'
   },
   {
     id: '55',
     name: 'CLEANROT KNIGHT FINLAY ASHES',
-    img: '../assets/CLEANROT_KNIGHT_FINLAY_ASHES.png',
+    img: './assets/CLEANROT_KNIGHT_FINLAY_ASHES.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Cleanrot+Knight+Finlay+Ashes'
   },
   {
     id: '56',
     name: 'BLACK KNIFE TICHE',
-    img: '../assets/BLACK_KNIFE_TICHE.png',
+    img: './assets/BLACK_KNIFE_TICHE.png',
     dif: '0',
     Link: 'https://eldenring.wiki.fextralife.com/Black+Knife+Tiche+Ashes'
   },
   {
     id: '57',
     name: 'MIMIC TEAR ASHES',
-    img: '../assets/MIMIC_TEAR_ASHES.png',
+    img: './assets/MIMIC_TEAR_ASHES.png',
     dif: '0',
     Link: 'https://eldenring.wiki.fextralife.com/Mimic+Tear+Ashes'
   },
   {
     id: '58',
     name: 'FINGER MAIDEN THEROLINA PUPPET',
-    img: '../assets/FINGER_MAIDEN_THEROLINA_PUPPET.png',
+    img: './assets/FINGER_MAIDEN_THEROLINA_PUPPET.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Finger+Maiden+Therolina+Puppet'
   },
   {
     id: '59',
     name: 'JARWIGHT PUPPET',
-    img: '../assets/JARWIGHT_PUPPET.png',
+    img: './assets/JARWIGHT_PUPPET.png',
     dif: '2',
     Link: 'https://eldenring.wiki.fextralife.com/Jarwight+Puppet'
   },
   {
     id: '60',
     name: 'DOLORES THE SLEEPING ARROW PUPPET',
-    img: '../assets/DOLORES.png',
+    img: './assets/DOLORES.png',
     dif: '3',
     Link: 'https://eldenring.wiki.fextralife.com/Dolores+the+Sleeping+Arrow+Puppet'
   },
   {
     id: '61',
     name: 'NEPHELI LOUX PUPPET',
-    img: '../assets/NEPHELI_LOUX_PUPPET.png',
+    img: './assets/NEPHELI_LOUX_PUPPET.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Nepheli+Loux+Puppet'
   },
   {
     id: '62',
     name: 'DUNG EATER PUPPET',
-    img: '../assets/DUNG_EATER_PUPPET.png',
+    img: './assets/DUNG_EATER_PUPPET.png',
     dif: '1',
     Link: 'https://eldenring.wiki.fextralife.com/Dung+Eater+Puppet'
   },
   {
     id: '63',
     name: 'NIGHTMAIDEN & SWORDSTRESS PUPPETS',
-    img: '../assets/NIGHTMAIDEN_SWORDSTRESS_PUPPETS.png',
+    img: './assets/NIGHTMAIDEN_SWORDSTRESS_PUPPETS.png',
     dif: '5',
     Link: 'https://eldenring.wiki.fextralife.com/Nightmaiden+&+Swordstress+Puppets'
   }
